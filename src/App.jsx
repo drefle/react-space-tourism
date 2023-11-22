a52c24790ecf963a89e7020f3b21b7a106e244b0
David <you@example.com>
2023-11-22 14:49:43 +0100

    Commit of the project

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,11 @@ function App() {
         <BrowserRouter>
         <div className="absolute z-30 w-full p-6 md:p-0">
           <header className="flex justify-between items-center lg:pt-[40px] lg:pl-[31px]">
-            <img className="md:pt-6 md:pl-10 lg:p-6" src="../static/assets/shared/logo.svg" alt="logo space tourism" />
+            <img className="md:pt-6 md:pl-10 lg:p-6" src={`/shared/logo.svg`} alt="logo space tourism" />
             <div className=" hidden bg-white opacity-25 lg:h-[1px] lg:w-10 lg:block lg:grow lg:translate-x-10 lg:z-20"></div>
-            <button className="md:hidden" onClick={()=>setShowMenu(true)}><img id="menu-hamburger" src="../static/assets/shared/icon-hamburger.svg" alt="logo menu" /></button>
+            <button className="md:hidden" onClick={()=>setShowMenu(true)}><img id="menu-hamburger" src={`/shared/icon-hamburger.svg`} alt="logo menu" /></button>
             <nav id="navigation" className={`${showMenu?"translate-x-[0]":"translate-x-[100%]"} flex flex-col absolute transition-all duration-700 top-0 right-0 bg-white/5 backdrop-blur-2xl h-[100vh] min-w-[250px] pl-8 md:translate-x-0 md:relative md:min-w-[60%] md:h-fit lg:pl-[123px]`}>
-                <button className="ml-auto m-8 flex items-center md:hidden" onClick={()=>setShowMenu(false)}><img src="../static/assets/shared/icon-close.svg" alt="logo close menu" /></button>
+                <button className="ml-auto m-8 flex items-center md:hidden" onClick={()=>setShowMenu(false)}><img src={`/shared/icon-close.svg`} alt="logo close menu" /></button>
                 <ul className="flex pt-[8vh] gap-8 flex-col text-left font-barlow text-mainLight tracking-[2.7px] md:flex-row md:items-center md:pt-0 md:h-full lg:gap-12">
                 <li className="flex items-center">
                     <NavLink className={({isActive})=>`${isActive && "border-r-mainLight border-r-[3px] md:border-b-[3px] md:border-b-mainLight"} py-3 flex grow border-b-transparent hover:border-r-[3px] hover:border-r-mainLight/50 md:py-9 md:border-r-0 md:border-b-[3px] md:hover:border-b-mainLight/50 md:hover:border-r-0`} to="/"><span className="mr-[11px] font-bold md:hidden lg:block">00</span><span>HOME</span></NavLink>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,18 +13,18 @@ export default {
         "barlow":['Barlow Condensed', 'sans-serif']
       },
       backgroundImage:{
-        'home-mobile':"url(./static/assets/home/background-home-mobile.jpg)",
-        'home-tablet':"url(./static/assets/home/background-home-tablet.jpg)",
-        'home-desktop':"url(./static/assets/home/background-home-desktop.jpg)",
-        'destination-mobile':"url(./static/assets/destination/background-destination-mobile.jpg)",
-        'destination-tablet':"url(./static/assets/destination/background-destination-tablet.jpg)",
-        'destination-desktop':"url(./static/assets/destination/background-destination-desktop.jpg)",
-        'crew-mobile':"url(./static/assets/crew/background-crew-mobile.jpg)",
-        'crew-tablet':"url(./static/assets/crew/background-crew-tablet.jpg)",
-        'crew-desktop':"url(./static/assets/crew/background-crew-desktop.jpg)",
-        'technology-mobile':"url(./static/assets/technology/background-technology-mobile.jpg)",
-        'technology-tablet':"url(./static/assets/technology/background-technology-tablet.jpg)",
-        'technology-desktop':"url(./static/assets/technology/background-technology-desktop.jpg)"
+        'home-mobile':"url(/home/background-home-mobile.jpg)",
+        'home-tablet':"url(/home/background-home-tablet.jpg)",
+        'home-desktop':"url(/home/background-home-desktop.jpg)",
+        'destination-mobile':"url(/destination/background-destination-mobile.jpg)",
+        'destination-tablet':"url(/destination/background-destination-tablet.jpg)",
+        'destination-desktop':"url(/destination/background-destination-desktop.jpg)",
+        'crew-mobile':"url(/crew/background-crew-mobile.jpg)",
+        'crew-tablet':"url(/crew/background-crew-tablet.jpg)",
+        'crew-desktop':"url(/crew/background-crew-desktop.jpg)",
+        'technology-mobile':"url(/technology/background-technology-mobile.jpg)",
+        'technology-tablet':"url(/technology/background-technology-tablet.jpg)",
+        'technology-desktop':"url(/technology/background-technology-desktop.jpg)"
       }
     },
   },
